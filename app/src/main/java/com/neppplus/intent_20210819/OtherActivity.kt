1c438b5f8c184be5113e440abf0ea7a7c29c7ef9
조경진 <cho881020@gmail.com>
2021-08-19 15:33:49 +0900

    이전 화면으로 복귀하기 (finish)

--- app/src/main/java/com/neppplus/intent_20210819/OtherActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210819/OtherActivity.kt
@@ -1,11 +1,24 @@
 package com.neppplus.intent_20210819
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_other.*
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+
+        returnToMainBtn.setOnClickListener {
+
+//            val myIntent = Intent(this, MainActivity::class.java)
+//            startActivity(myIntent)
+
+//            이전화면 복귀 : 현재 화면을 종료.
+            finish()
+
+        }
+
     }
 }
