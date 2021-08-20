d9048bc2bebc4f6cf77792f2552347bb51b2bde2
조경진 <cho881020@gmail.com>
2021-08-20 09:58:56 +0900

    카카오톡 다운로드 링크 예제

--- app/src/main/java/com/neppplus/intent_20210819/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210819/MainActivity.kt
@@ -16,6 +16,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        kakaoStoreBtn.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW, myUri )
+            startActivity(myIntent)
+        }
+
         naverBtn.setOnClickListener {
             val myUri = Uri.parse("https://naver.com")
             val myIntent = Intent( Intent.ACTION_VIEW, myUri )
